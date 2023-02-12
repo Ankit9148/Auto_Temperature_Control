17acfe148a563d66225f2504772e643218c492fa
Ankit9148 <ankit.chanda94@gmail.com>
2023-02-12 14:02:13 +0100

    Create new cmake project

---
+++ AutoTempControl/AutoTempControl/AutoTempControl.cpp
@@ -0,0 +1,12 @@
+﻿// AutoTempControl.cpp : Defines the entry point for the application.
+//
+
+#include "AutoTempControl.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
