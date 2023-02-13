dc47c74380b7560742d7e7296e6eba8b9b733aa3
Ankit9148 <ankit.chanda@mail.polimi.it>
2023-02-14 00:22:27 +0100

    Shift methods from main

--- AutoTempControl/AutoTempControl/AutoTempControlMain.cpp
+++ AutoTempControl/AutoTempControl/AutoTempControlMain.cpp
@@ -4,70 +4,12 @@
 #include "AutoTempControlMain.h"
 
 using namespace std;
-#define COOLER_START_TEMP 35
-#define COOLER_STOP_TEMP 25
-#define HEATER_START_TEMP 15
-#define HEATER_STOP_TEMP 20
+
 int main()
 {
-
 	timer_start(intitMianThread, 1000);
 
 	while (true);
-	/*
-	UserTemperatureSetting myTempSetting;
-	TempSensor mySensorTemp;
-	int startTemp, stopTemp = 0;
-
-	cout << "Heater Temperature Setting" << endl;
-	cout << "Set Heater Start Temperature" << endl;
-	cin >> startTemp;
-	cout << "Set Heater Stop Temperature" << endl;
-	cin >> stopTemp;
-	while (startTemp > stopTemp)
-	{
-		cout << "Stop Temperature should be greater than start temperature" << endl;
-		cout << "Set Heater Stop Temperature" << endl;
-		cin >> stopTemp;
-	}
-	myTempSetting.setHeaterTemperature(startTemp, stopTemp);
-
-	cout << "Cooler Temperature Setting" << endl;
-	cout << "Set Cooler Start Temperature" << endl;
-	cin >> startTemp;
-	cout << "Set Cooler Stop Temperature" << endl;
-	cin >> stopTemp;
-	while (startTemp < stopTemp)
-	{
-		cout << "Start temperature should be greater than stop temperature" << endl;
-		cout << "Set Cooler Stop Temperature" << endl;
-		cin >> stopTemp;
-	}
-	myTempSetting.setCoolerTemperature(startTemp, stopTemp);
-
-	int roomTemp = mySensorTemp.getTemperature();
-
-	if (roomTemp < HEATER_START_TEMP)
-	{
-		cout << "Start Heater" << endl;
-		while (roomTemp <= HEATER_STOP_TEMP)
-		{
-			roomTemp++;
-			Sleep(1000);
-		}
-		cout << "Room Temperature Has reached: " << roomTemp << endl;
-	}
-	else if (roomTemp > COOLER_START_TEMP)
-	{
-		cout << "Started Cooling" << endl;
-		while (roomTemp >= COOLER_STOP_TEMP)
-		{
-			roomTemp--;
-			Sleep(1000);
-		}
-		cout << "Room Temperature Has reached: " << roomTemp << endl;
-	}*/
-
 	return 0;
 }
 
@@ -87,3 +29,4 @@ void intitMianThread()
 {
 	handler_1Sec();
 }
+
--- AutoTempControl/AutoTempControl/AutoTempControlMain.h
+++ AutoTempControl/AutoTempControl/AutoTempControlMain.h
@@ -8,11 +8,8 @@
 #include <chrono>
 #include <thread>
 #include <functional>
-#include "TempSensor.h"
-#include "UserSetting.h"
 #include "ApplianceController.h"
 
 void timer_start(std::function<void(void)> func, unsigned int interval);
 void intitMianThread();
-
 // TODO: Reference additional headers your program requires here.
