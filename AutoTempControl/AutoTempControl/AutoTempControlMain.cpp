bb09d44df1de069a0cfa6d8016a587e908621668
Ankit9148 <ankit.chanda@mail.polimi.it>
2023-02-16 00:35:10 +0100

    Create two separate threads to run FSM and UI controller

--- AutoTempControl/AutoTempControl/AutoTempControlMain.cpp
+++ AutoTempControl/AutoTempControl/AutoTempControlMain.cpp
@@ -7,8 +7,9 @@ using namespace std;
 
 int main()
 {
-	timer_start(intitMianThread, 1000);
-
+	//timer_start(intitMianThread, 1000);
+	std::thread mainApplication(intitMianThread);
+    std::thread uiApplication(initUiThread);
 	while (true);
 	return 0;
 }
@@ -27,6 +28,19 @@ void timer_start(std::function<void(void)> func, unsigned int interval)
 
 void intitMianThread()
 {
-	handler_1Sec();
+	while (true)
+        {
+            handler_1Sec();
+            std::this_thread::sleep_for(1000ms);
+        }
 }
+
+void initUiThread()
+{
+    while (true)
+    {
+        userOption();
+    }
+}
+
 
--- AutoTempControl/AutoTempControl/AutoTempControlMain.h
+++ AutoTempControl/AutoTempControl/AutoTempControlMain.h
@@ -9,7 +9,9 @@
 #include <thread>
 #include <functional>
 #include "ApplianceController.h"
+#include "Application.h"
 
 void timer_start(std::function<void(void)> func, unsigned int interval);
 void intitMianThread();
+void initUiThread();
 // TODO: Reference additional headers your program requires here.
