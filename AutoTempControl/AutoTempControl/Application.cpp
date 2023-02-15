105aeb229e2d04f1c041e9ecb09db1690462dce1
Ankit9148 <ankit.chanda@mail.polimi.it>
2023-02-16 00:35:31 +0100

    Add UI to feature

--- AutoTempControl/AutoTempControl/Application.cpp
+++ AutoTempControl/AutoTempControl/Application.cpp
@@ -1,107 +1,21 @@
 #include "Application.h"
 using namespace std;
 
-int applicationRunningState = 0;
-bool isHeaterRunning, isCoolerRunning = false;
 static void initialize_userSetting();
 
-void applicationHandler()
-{
-    int roomTemp = getTemperature();
-    switch (applicationRunningState)
-    {
-    case STATE_INIT:
-        // Init Appliance
-        // cout << "Appliance State" << applianceControllerRunningState << endl;
-        applicationRunningState = STATE_USER_INPUT_START;
-        break;
-    case STATE_OFF:
-    {
-        if (getSeason() == SEASON_SUMMER)
-        {
-            roomTemp++;
-            setTemperatureSensor(roomTemp);
-            if (roomTemp > getCoolerStartTemp())
-            {
-                cout << "Room Temperature Has Gone UP: Staring Cooler....\n";
-                applicationRunningState = STATE_SUMMER_RUN;
-            }
-        }
-        else if (getSeason() == SEASON_WINTER)
-        {
-            roomTemp--;
-            setTemperatureSensor(roomTemp);
-            if (roomTemp < getHeaterStartTemp())
-            {
-                cout << "Room Temperature Has Fallen Down: Staring Heater....\n";
-                applicationRunningState = STATE_WINTER_RUN;
-            }
-        }
-    }
-    break;
-    case STATE_USER_INPUT_START:
-        applicationRunningState = STATE_USER_INPUT_WAIT;
-        // Configure specific appliance
+void userOption() {
+    int userInput;
+    cout << "Press 1: Config, 2: Exit\n";
+    cin >> userInput;
+    switch (userInput) {
+    case 1:
         initialize_userSetting();
         break;
-    case STATE_USER_INPUT_WAIT:
-        // wiat in this state until user input finishes
+    case 2:
+        exit(0);
         break;
-    case STATE_USER_INPUT_END:
-    {
-        // Initialize Peripherals
-        if (getSeason() == SEASON_SUMMER)
-        {
-            if (roomTemp > getCoolerStartTemp())
-            {
-                cout << "Room Temperature Has Gone UP: Staring Cooler....\n";
-                applicationRunningState = STATE_SUMMER_RUN;
-            }
-            else
-            {
-                cout << "Room temperature is moderate\n";
-                applicationRunningState = STATE_OFF;
-            }
-        }
-        else if (getSeason() == SEASON_WINTER)
-        {
-            if (roomTemp < getHeaterStartTemp())
-            {
-                cout << "Room Temperature Has Fallen Down: Staring Heater....\n";
-                applicationRunningState = STATE_WINTER_RUN;
-            }
-            else
-            {
-                cout << "Room temperature is moderate\n";
-                applicationRunningState = STATE_OFF;
-            }
-        }
-    }
-    break;
-    case STATE_WINTER_RUN:
-    {
-        roomTemp++;
-        setTemperatureSensor(roomTemp);
-        if (roomTemp >= getHeaterStopTemp())
-        {
-            cout << "Room Temperature Has reached: " << roomTemp << ". Turning OFF Heater\n";
-            applicationRunningState = STATE_OFF;
-        }
-    }
-    break;
-    case STATE_SUMMER_RUN:
-    {
-        roomTemp--;
-        setTemperatureSensor(roomTemp);
-        if (roomTemp <= getCoolerStopTemp())
-        {
-            cout << "Room Temperature Has reached: " << roomTemp << ". Turning OFF Cooler\n";
-            applicationRunningState = STATE_OFF;
-        }
-    }
-    break;
     default:
-        break;
+        cout << "Invalid Input\n";
     }
 }
 
@@ -146,5 +60,6 @@ static void initialize_userSetting()
     cin >> seasonSetting;
     setSeason(seasonSetting);
 
-    applicationRunningState = STATE_USER_INPUT_END;
+    setRunnerState(STATE_USER_INPUT_END);
+    //applicationRunningState = STATE_USER_INPUT_END;
 }
--- AutoTempControl/AutoTempControl/Application.h
+++ AutoTempControl/AutoTempControl/Application.h
@@ -2,22 +2,6 @@
 #include <iostream>
 #include "TempSensor.h"
 #include "UserSetting.h"
+#include "Runner.h"
 
-enum ApplianceState
-{
-    STATE_INIT,
-    STATE_OFF,
-    STATE_USER_INPUT_START,
-    STATE_USER_INPUT_WAIT,
-    STATE_USER_INPUT_END,
-    STATE_WINTER_RUN,
-    STATE_SUMMER_RUN
-};
-
-enum SeasonType
-{
-    SEASON_SUMMER,
-    SEASON_WINTER
-};
-
-void applicationHandler();
+void userOption();
